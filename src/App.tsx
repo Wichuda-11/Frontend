4f8a85c2a8775a2ed86dcac84e67da0b768e9b19
Jib <84692823+Wichuda-11@users.noreply.github.com>
2023-06-15 12:23:22 +0700

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App-header">
+      <header className="App">
+        <div>
+          <Button className="rotate-button">
+            <div className="triangle"></div>
+          </Button>
+          <Button className="rotate-button2">
+            <div className="triangle"></div>
+          </Button>
+          <Button className="rotate-button3">
+            <div className="triangle"></div>
+          </Button>
+          <Button className="rotate-button4">
+            <div className="triangle"></div>
+          </Button>
+        </div>
       </header>
+      <header>
+        <Button className="rectangle">
+          <div></div>
+        </Button>
+        <Button className="circle">
+          <div></div>
+        </Button>
+        <Button className="circle2">
+          <div></div>
+        </Button>
+
+        <br></br>
+        <Button className="rectangle2"></Button>
+        <Button className="rectangle3"></Button>
+        <Button className="rectangle4"></Button>
+      </header>
+
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a>Learn React</a>
     </div>
   );
 }
